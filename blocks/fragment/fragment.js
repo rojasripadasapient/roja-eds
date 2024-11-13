eeac55510382c35e7f30f564ee06308ac5eb8a64
LL\rojsripa <roja.sripada@publicissapient.com>
2024-11-13 23:49:44 +0530

    test1

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -56,3 +56,23 @@ export default async function decorate(block) {
     }
   }
 }
+// media-gallery
+(function(document, $) {
+    $(document).on("foundation-contentloaded", function() {
+        const teaserSelect = $('[name="./teaser"]');
+
+        function toggleFields() {
+            const selectedVariant = teaserSelect.val();
+            $('[data-show-for-variant]').each(function() {
+                const variant = $(this).data('show-for-variant');
+                $(this).closest(".coral-Form-fieldwrapper").toggle(variant === selectedVariant);
+            });
+        }
+
+        // Initial check on load
+        toggleFields();
+
+        // Event listener for teaser dropdown change
+        teaserSelect.on("change", toggleFields);
+    });
+})(document, Granite.$);
