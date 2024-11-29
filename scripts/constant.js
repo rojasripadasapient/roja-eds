7835ddaa33466ac4ebfa9c8bc446c5db8bbca4e3
LL\rojsripa <roja.sripada@publicissapient.com>
2024-11-29 16:06:18 +0530

    test

---
+++ scripts/constant.js
@@ -0,0 +1,18 @@
+const BLOCK_FOLDER_PATH = {
+  'blocks/global': ['header', 'footer', 'cards', 'columns', 'fragment', 'hero', 'title', 'tab'],
+  'blocks/global/common': ['action-strip', 'image'],
+  'blocks/global/editorial': ['hero-intro', 'content-promo', 'accordion', 'teaser'],
+  'blocks/global/navigation': ['sticky-navigation'],
+};
+
+const CONTENT_PROMO_VARIATION = {
+  VARIANT_1: 'variation1',
+  VARIANT_2: 'variation2',
+};
+
+const TEASER_BLOCK_VARIATION = {
+  VARIANT_1: 'var1',
+  VARIANT_2: 'var2',
+};
+
+export { BLOCK_FOLDER_PATH, CONTENT_PROMO_VARIATION, TEASER_BLOCK_VARIATION };
